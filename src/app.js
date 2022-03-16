00f5dd343e72bc4cff6083ba67643c12e72a1ca3
Agustin Enriquez <agustinenriquez12@gmail.com>
2022-03-15 22:40:01 -0300

    arreglo

--- src/app.js
+++ src/app.js
@@ -5,15 +5,15 @@ const PORT =process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname,"../","public")));
 
-app.get("/", (req,res) => res.sendFile (path.join(__dirname,"views", "home.html")));
+app.get("/", (req,res) => res.sendFile (path.join(__dirname,"views", "Home.html")));
 
 app.listen(PORT, () => console.log("Estoy funcionando"));
 
 
 app.get('/Login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
+    res.sendFile(path.join(__dirname, '/views/Login.html'));
 });
 
 app.get('/Register', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
+    res.sendFile(path.join(__dirname, '/views/Register.html'));
 });
